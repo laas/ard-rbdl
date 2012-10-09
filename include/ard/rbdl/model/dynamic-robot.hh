1a1eaf779c14c5de38d760b2c5b1b2a418932f46
Antonio El Khoury <aelkhour@laas.fr>
2012-10-09 19:59:43 +0200

    Add missing files for new class.

---
+++ include/ard/rbdl/model/dynamic-robot.hh
@@ -0,0 +1,358 @@
+// Copyright (C) 2012 by Antonio El Khoury.
+//
+// This file is part of the ard-rbdl.
+//
+// ard-rbdl is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// ard-rbdl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with ard-rbdl.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+/// \brief Declaration of DynamicRobot class.
+
+#ifndef ARD_RBDL_MODEL_DYNAMICROBOT_HH
+# define ARD_RBDL_MODEL_DYNAMICROBOT_HH
+
+# include <boost/enable_shared_from_this.hpp>
+
+# include <rbdl/Model.h>
+
+# include <ard/rbdl/tools/fwd.hh>
+# include <ard/rbdl/tools/types.hh>
+# include <ard/rbdl/model/joint.hh>
+# include <abstract-robot-dynamics/dynamic-robot.hh>
+
+namespace ard
+{
+  namespace rbdl
+  {
+    class DynamicRobot
+      : public CjrlDynamicRobot,
+	public boost::enable_shared_from_this<DynamicRobot>
+    {
+    public:
+      /// \name Initialization
+      /// \{
+
+      /// \brief Default constructor.
+      DynamicRobot ();
+
+      /// \brief Copy constructor.
+      DynamicRobot (const DynamicRobot& robot);
+
+      /// \brief Destructor
+      virtual ~DynamicRobot ();
+
+      /// \brief Initialize data-structure necessary to dynamic computations
+      /// This function should be called after building the tree of joints.
+      virtual bool initialize ();
+
+      /// \}
+
+      /// \name Kinematic chain
+      /// \{
+
+      /// \brief Set the root joint of the robot.
+      virtual void rootJoint (CjrlJoint& inJoint);
+
+      /// \brief Get the root joint of the robot.
+      virtual CjrlJoint* rootJoint () const;
+
+      /// \brief Get a vector containing all the joints.
+      virtual std::vector<CjrlJoint*> jointVector ();
+
+      /// \brief Get the chain of joints between two joints
+      /// \param inStartJoint First joint.
+      /// \param inEndJoint Second joint.
+      virtual std::vector<CjrlJoint*>
+      jointsBetween (const CjrlJoint& inStartJoint,
+		     const CjrlJoint& inEndJoint) const;
+
+      /// \brief Get the upper bound for ith dof.
+      virtual double upperBoundDof (unsigned int inRankInConfiguration);
+
+      /// \brief Get the lower bound for ith dof.
+      virtual double lowerBoundDof (unsigned int inRankInConfiguration);
+
+      /// \brief Compute the upper bound for ith dof using other
+      /// configuration values if possible.
+      virtual double upperBoundDof (unsigned int inRankInConfiguration,
+				    const vectorN& inConfig);
+
+      /// \brief Compute the lower bound for ith dof using other
+      /// configuration values if possible.
+      virtual double lowerBoundDof (unsigned int inRankInConfiguration,
+				    const vectorN& inConfig);
+
+      /// \brief Get the number of degrees of freedom of the robot.
+      virtual unsigned int numberDof () const;
+
+      /// \brief Set the joint ordering in the configuration vector
+      ///
+      /// \param inJointVector Vector of the robot joints
+      ///
+      /// Specifies the order of the joints in the configuration vector.
+      /// The vector should contain all the joints of the current robot.
+      virtual void setJointOrderInConfig (std::vector<CjrlJoint*> inJointVector);
+
+      /// \}
+
+      /// \name Configuration, velocity and acceleration
+
+      /// \brief Set the current configuration of the robot.
+      ///
+      /// \param inConfig the configuration vector \f${\bf q}\f$.
+      ///
+      /// \return true if success, false if failure (the dimension of the
+      /// input vector does not fit the number of degrees of freedom of the
+      /// robot).
+      virtual bool currentConfiguration (const vectorN& inConfig);
+
+      /// \brief Get the current configuration of the robot.
+      ///
+      /// \return the configuration vector \f${\bf q}\f$.
+      virtual const vectorN& currentConfiguration () const;
+
+      /// \brief Set the current velocity of the robot.
+      ///
+      /// \param inVelocity the velocity vector \f${\bf \dot{q}}\f$.
+      ///
+      /// \return true if success, false if failure (the dimension of the
+      /// input vector does not fit the number of degrees of freedom of the
+      /// robot).
+      virtual bool currentVelocity (const vectorN& inVelocity);
+
+      /// \brief Get the current velocity of the robot.
+      ///
+      /// \return the velocity vector \f${\bf \dot{q}}\f$.
+      virtual const vectorN& currentVelocity () const;
+
+      /// \brief Set the current acceleration of the robot.
+      ///
+      /// \param inAcceleration the acceleration vector \f${\bf \ddot{q}}\f$.
+      ///
+      /// \return true if success, false if failure (the dimension of the
+      /// input vector does not fit the number of degrees of freedom of the
+      /// robot).
+      virtual bool currentAcceleration (const vectorN& inAcceleration);
+
+      /// \brief Get the current acceleration of the robot.
+      ///
+      /// \return the acceleration vector \f${\bf \ddot{q}}\f$.
+      virtual const vectorN& currentAcceleration () const;
+
+      /// \brief Get the current forces of the robot.
+      ///
+      /// \return the force vector \f${\bf f}\f$.
+      virtual const matrixNxP& currentForces () const;
+
+      /// \brief Get the current torques of the robot.
+      ///
+      /// \return the torque vector \f${\bf \tau }\f$.
+      virtual const matrixNxP& currentTorques () const;
+
+      /// \brief Get the current joint torques of the robot.
+      ///
+      /// The torques are computed by internal calls to the
+      /// direct dynamic computations. Dynamics is computed in free-floating
+      /// mode, supposing no contact with the environments, and knowing
+      /// given position, velocity and acceleration. This accessor only give
+      /// a reference on the already-computed values.
+      /// \return the torque vector \f${\bf \tau }\f$.
+      virtual const vectorN& currentJointTorques () const;
+
+      /// \}
+
+      /// \name Forward kinematics and dynamics
+
+      /// \brief Compute forward kinematics.
+      ///
+      /// Update the position, velocity and accelerations of each
+      /// joint wrt \f${\bf {q}}\f$, \f${\bf \dot{q}}\f$, \f${\bf \ddot{q}}\f$.
+      virtual bool computeForwardKinematics ();
+
+      /// \brief Compute the dynamics of the center of mass.
+      ///
+      /// Compute the linear and angular momentum and their time
+      /// derivatives, at the center of mass.
+      virtual bool computeCenterOfMassDynamics ();
+
+      /// \brief Get the position of the center of mass.
+      virtual const vector3d& positionCenterOfMass () const;
+
+      /// \brief Get the velocity of the center of mass.
+      virtual const vector3d& velocityCenterOfMass ();
+
+      /// \brief Get the acceleration of the center of mass.
+      virtual const vector3d& accelerationCenterOfMass ();
+
+      /// \brief Get the linear momentum of the robot.
+      virtual const vector3d& linearMomentumRobot ();
+
+      /// \brief Get the time-derivative of the linear momentum.
+      virtual const vector3d& derivativeLinearMomentum ();
+
+      /// \brief Get the angular momentum of the robot at the center of mass.
+      virtual const vector3d& angularMomentumRobot ();
+
+      /// \brief Get the time-derivative of the angular momentum at the
+      /// center of mass.
+      virtual const vector3d& derivativeAngularMomentum ();
+
+      /// \brief Get the total mass of the robot
+      virtual double mass () const;
+
+      /// \}
+
+      /// \name Control of the implementation
+      /// \{
+
+      /// \brief Whether the specified property in implemented.
+      virtual bool isSupported (const std::string &);
+
+      /// \brief Get property corresponding to command name.
+      ///
+      /// \param inProperty name of the property.
+      /// \retval outValue value of the property if implemented.
+      ///
+      /// \note The returned string needs to be cast into the right type
+      /// (double, int,...).
+      virtual bool getProperty (const std::string &,
+				std::string&) const;
+
+      /// \brief Set property corresponding to command name.
+      ///
+      /// \param inProperty name of the property.
+      /// \param inValue value of the property.
+      ///
+      /// \note The value string is obtained by writing the
+      /// corresponding value in a string (operator<<).
+      virtual bool setProperty (std::string &,
+				const std::string&);
+
+      /// \}
+
+
+      /// \brief Compute and get position and orientation jacobian
+      ///
+      /// \param inStartJoint First joint in the chain of joints
+      /// influencing the jacobian.
+      /// \param inEndJoint Joint where the control frame is located.
+      /// \param inFrameLocalPosition Position of the control frame in
+      /// inEndJoint local frame.
+      /// \retval outjacobian computed jacobian matrix.
+      /// \param offset is the rank of the column from where the
+      /// jacobian is written.
+      /// \param inIncludeStartFreeFlyer Option to include the
+      /// contribution of a fictive freeflyer superposed with
+      /// inStartJoint
+      ///
+      /// \return false if matrix has inadequate size. Number of
+      /// columns in matrix outJacobian must be at least numberDof()
+      /// if inIncludeStartFreeFlyer = true.
+      /// It must be at least numberDof()-6 otherwise.
+      /// 
+      virtual bool getJacobian(const CjrlJoint& inStartJoint,
+			       const CjrlJoint& inEndJoint,
+			       const vector3d& inFrameLocalPosition,
+			       matrixNxP& outjacobian,
+			       unsigned int offset = 0,
+			       bool inIncludeStartFreeFlyer = true);
+
+      virtual bool getPositionJacobian(const CjrlJoint& inStartJoint,
+				       const CjrlJoint& inEndJoint,
+				       const vector3d& inFrameLocalPosition,
+				       matrixNxP& outjacobian,
+				       unsigned int offset = 0,
+				       bool inIncludeStartFreeFlyer = true);
+
+      virtual bool getOrientationJacobian(const CjrlJoint& inStartJoint,
+					  const CjrlJoint& inEndJoint,
+					  matrixNxP& outjacobian,
+					  unsigned int offset = 0,
+					  bool inIncludeStartFreeFlyer = true);
+
+      virtual bool getJacobianCenterOfMass(const CjrlJoint& inStartJoint,
+					   matrixNxP& outjacobian,
+					   unsigned int offset = 0,
+					   bool inIncludeStartFreeFlyer = true);
+
+      ///\name Inertia matrix related methods
+      /// \{
+
+      /// \brief Compute the inertia matrix of the robot according wrt
+      /// \f${\bf q}\f$.
+      virtual void computeInertiaMatrix ();
+
+      /// \brief Get the inertia matrix of the robot according wrt \f${\bf q}\f$.
+      virtual const matrixNxP& inertiaMatrix () const;
+      /// \}
+
+      /// \name Actuated joints related methods.
+      /// \{
+
+      /// \brief Returns the list of actuated joints.
+      virtual const std::vector<CjrlJoint*>& getActuatedJoints() const;
+
+      /// \brief Specifies the list of actuated joints.
+      virtual void setActuatedJoints(std::vector<CjrlJoint*>& lActuatedJoints);
+
+      /// \}
+
+    protected:
+      /// \brief Set the root joint of the robot.
+      virtual void rootJoint (joint_t& joint);
+
+    private:
+      /// \brief Root joint pointer attribute.
+      jointShPtr_t rootJoint_;
+      /// \brief Joint vector attribute.
+      jointWkPtrs_t jointVector_;
+      /// \brief rbdl model attribute.
+      rbdlModel_t rbdlModel_;
+      /// \brief configuration attribute.
+      vectorN configuration_;
+      /// \brief velocity attribute.
+      vectorN velocity_;
+      /// \brief acceleration attribute.
+      vectorN acceleration_;
+      /// \brief forces attribute.
+      vectorN forces_;
+      /// \brief torques attribute.
+      vectorN torques_;
+      /// \brief jointTorques attribute.
+      vectorN jointTorques_;
+      /// \brief center of mass position attribute.
+      vector3d positionCenterOfMass_;
+      /// \brief center of mass velocity attribute.
+      vector3d velocityCenterOfMass_;
+      /// \brief center of mass acceleration attribute.
+      vector3d accelerationCenterOfMass_;
+      /// \brief linear momentum attribute.
+      vector3d linearMomentumRobot_;
+      /// \brief linear momentum derivative attribute.
+      vector3d derivativeLinearMomentum_;
+      /// \brief angular momentum attribute.
+      vector3d angularMomentumRobot_;
+      /// \brief angular momentum derivative attribute.
+      vector3d derivativeAngularMomentum_;
+      /// \brief mass attribute.
+      double mass_;
+      /// \brief inertia matrix attribute.
+      matrixNxP inertiaMatrix_;
+      /// \brief actuated joints attribute.
+      jointWkPtrs_t actuatedJoints_;
+    };
+
+  } // end of namespace rbdl.
+} // end of namespace ard.
+
+#endif //! ARD_RBDL_MODEL_DYNAMICROBOT_HH
---
+++ src/dynamic-robot.cc
@@ -0,0 +1,368 @@
+// Copyright (C) 2012 by Antonio El Khoury.
+//
+// This file is part of the ard-rbdl.
+//
+// ard-rbdl is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// ard-rbdl is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with ard-rbdl.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+/// \file src/model/dynamic-robot.cc
+/// \brief Implementation of DynamicRobot.
+
+#include <stdexcept>
+
+#include <boost/foreach.hpp>
+
+#include <ard/rbdl/tools/util.hh>
+#include <ard/rbdl/model/dynamic-robot.hh>
+
+namespace ard
+{
+  namespace rbdl
+  {
+    DynamicRobot::DynamicRobot () :
+      boost::enable_shared_from_this<DynamicRobot> (),
+      rootJoint_ (),
+      jointVector_ (),
+      rbdlModel_ (),
+      configuration_ (),
+      velocity_ (),
+      acceleration_ (),
+      forces_ (),
+      torques_ (),
+      jointTorques_ (),
+      positionCenterOfMass_ (),
+      velocityCenterOfMass_ (),
+      accelerationCenterOfMass_ (),
+      linearMomentumRobot_ (),
+      derivativeLinearMomentum_ (),
+      angularMomentumRobot_ (),
+      derivativeAngularMomentum_ (),
+      mass_ (),
+      inertiaMatrix_ (),
+      actuatedJoints_ ()
+    {
+    }
+
+    DynamicRobot::~DynamicRobot ()
+    {
+    }
+
+    void DynamicRobot::rootJoint (CjrlJoint& joint)
+    {
+      jointPtr_t ptr = dynamic_cast<jointPtr_t> (&joint);
+      if (ptr)
+	rootJoint (*ptr);
+      else
+	throw std::runtime_error ("Null pointer to joint.");
+    }
+
+    CjrlJoint* DynamicRobot::rootJoint () const
+    {
+      return getUnsafePointer (rootJoint_);
+    }
+
+    std::vector<CjrlJoint*> DynamicRobot::jointVector ()
+    {
+      ardJointPtrs_t ardJoints (jointVector_.size ());
+      for (unsigned i = 0; i < jointVector_.size (); ++i)
+	ardJoints[i] = getUnsafePointer (jointVector_[i]);
+      return ardJoints;
+    }
+
+    std::vector<CjrlJoint*>
+    DynamicRobot::jointsBetween (const CjrlJoint& startJoint,
+				 const CjrlJoint& endJoint) const
+    {
+      ardJointPtrs_t robotRoot2StartJoint = startJoint.jointsFromRootToThis ();
+      ardJointPtrs_t robotRoot2EndJoint = endJoint.jointsFromRootToThis ();
+
+      unsigned int i;
+      unsigned int lastCommonJointRank = 0;
+      unsigned int minChainLength
+	= (robotRoot2StartJoint.size () < robotRoot2EndJoint.size())
+	? robotRoot2StartJoint.size() : robotRoot2EndJoint.size();
+
+      for (i = 1;i < minChainLength; ++i)
+	if ((robotRoot2StartJoint[i] == robotRoot2EndJoint[i]))
+	  ++lastCommonJointRank;
+
+      ardJointPtrs_t outJoints;
+      for (i = robotRoot2StartJoint.size() - 1; i > lastCommonJointRank; i--)
+	outJoints.push_back (robotRoot2StartJoint[i]);
+      if (lastCommonJointRank == 0)
+	outJoints.push_back (robotRoot2EndJoint[0]);
+      for (i = lastCommonJointRank + 1; i < robotRoot2EndJoint.size (); i++)
+	outJoints.push_back (robotRoot2EndJoint[i]);
+
+      return outJoints;
+    }
+
+    double DynamicRobot::upperBoundDof (unsigned int rankInConfiguration)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    double DynamicRobot::lowerBoundDof (unsigned int rankInConfiguration)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    double DynamicRobot::upperBoundDof (unsigned int rankInConfiguration,
+					const vectorN& config)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    double DynamicRobot::lowerBoundDof (unsigned int rankInConfiguration,
+					const vectorN& config)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    unsigned int DynamicRobot::numberDof () const
+    {
+      return rbdlModel_.dof_count;
+    }
+
+    void
+    DynamicRobot::setJointOrderInConfig (std::vector<CjrlJoint*> jointVector)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    bool DynamicRobot::currentConfiguration (const vectorN& config)
+    {
+      if (config.size () == numberDof ())
+	{
+	  configuration_ = config;
+	  return true;
+	}
+      else
+	return false;
+    }
+
+    const vectorN& DynamicRobot::currentConfiguration () const
+    {
+      return configuration_;
+    }
+
+    bool DynamicRobot::currentVelocity (const vectorN& velocity)
+    {
+      if (velocity.size () == numberDof ())
+	{
+	  velocity_ = velocity;
+	  return true;
+	}
+      else
+	return false;
+    }
+
+    const vectorN& DynamicRobot::currentVelocity () const
+    {
+      return velocity_;
+    }
+
+    bool DynamicRobot::currentAcceleration (const vectorN& acceleration)
+    {
+      if (acceleration.size () == numberDof ())
+	{
+	  acceleration_ = acceleration;
+	  return true;
+	}
+      else
+	return false;
+    }
+
+    const vectorN& DynamicRobot::currentAcceleration () const
+    {
+      return acceleration_;
+    }
+
+    const matrixNxP& DynamicRobot::currentForces () const
+    {
+      return forces_;
+    }
+
+    const matrixNxP& DynamicRobot::currentTorques () const
+    {
+      return torques_;
+    }
+
+    const vectorN& DynamicRobot::currentJointTorques () const
+    {
+      return jointTorques_;
+    }
+
+    bool DynamicRobot::computeForwardKinematics ()
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    bool DynamicRobot::computeCenterOfMassDynamics ()
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    const vector3d& DynamicRobot::positionCenterOfMass () const
+    {
+      return positionCenterOfMass_;
+    }
+
+    const vector3d& DynamicRobot::velocityCenterOfMass ()
+    {
+      return velocityCenterOfMass_;
+    }
+
+    const vector3d& DynamicRobot::accelerationCenterOfMass ()
+    {
+      return accelerationCenterOfMass_;
+    }
+
+    const vector3d& DynamicRobot::linearMomentumRobot ()
+    {
+      return linearMomentumRobot_;
+    }
+
+    const vector3d& DynamicRobot::derivativeLinearMomentum ()
+    {
+      return derivativeLinearMomentum_;
+    }
+
+    const vector3d& DynamicRobot::angularMomentumRobot ()
+    {
+      return angularMomentumRobot_;
+    }
+
+    const vector3d& DynamicRobot::derivativeAngularMomentum ()
+    {
+      return derivativeAngularMomentum_;
+    }
+
+    double DynamicRobot::mass () const
+    {
+      return mass_;
+    }
+
+    bool DynamicRobot::isSupported (const std::string& key)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    bool DynamicRobot::getProperty (const std::string & key,
+				    std::string& value) const
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    bool DynamicRobot::setProperty (std::string& key,
+				    const std::string& value)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    bool
+    DynamicRobot::getJacobian (const CjrlJoint& inStartJoint,
+			       const CjrlJoint& inEndJoint,
+			       const vector3d& inFrameLocalPosition,
+			       matrixNxP& outjacobian,
+			       unsigned int offset,
+			       bool inIncludeStartFreeFlyer)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    bool
+    DynamicRobot::getPositionJacobian (const CjrlJoint& inStartJoint,
+				       const CjrlJoint& inEndJoint,
+				       const vector3d& inFrameLocalPosition,
+				       matrixNxP& outjacobian,
+				       unsigned int offset,
+				       bool inIncludeStartFreeFlyer)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    bool
+    DynamicRobot::getOrientationJacobian (const CjrlJoint& inStartJoint,
+					  const CjrlJoint& inEndJoint,
+					  matrixNxP& outjacobian,
+					  unsigned int offset,
+					  bool inIncludeStartFreeFlyer)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    bool DynamicRobot::getJacobianCenterOfMass (const CjrlJoint& inStartJoint,
+						matrixNxP& outjacobian,
+						unsigned int offset,
+						bool inIncludeStartFreeFlyer)
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");      
+    }
+
+    void DynamicRobot::computeInertiaMatrix ()
+    {
+      // We assume this method will not be called.
+      throw std::runtime_error ("Method not supported.");
+    }
+
+    const matrixNxP& DynamicRobot::inertiaMatrix () const
+    {
+      return inertiaMatrix_;
+    }
+
+    const std::vector<CjrlJoint*>& DynamicRobot::getActuatedJoints () const
+    {
+      ardJointPtrs_t ardJoints (actuatedJoints_.size ());
+      for (unsigned i = 0; i < actuatedJoints_.size (); ++i)
+	ardJoints[i] = getUnsafePointer (actuatedJoints_[i]);
+      return ardJoints;
+    }
+ 
+    void
+    DynamicRobot::setActuatedJoints (std::vector<CjrlJoint*>& actuatedJoints)
+    {
+      actuatedJoints_.clear ();
+      BOOST_FOREACH (ardJointPtr_t ardJoint, actuatedJoints)
+	{
+	  jointPtr_t jointPtr = dynamic_cast<jointPtr_t> (ardJoint);
+	  if (jointPtr)
+	    actuatedJoints_.push_back (jointWkPtr_t
+				       (jointPtr->shared_from_this ()));
+	  else
+	    throw std::runtime_error ("Null pointer to joint.");
+	}
+    }
+
+    void DynamicRobot::rootJoint (joint_t& joint)
+    {
+      rootJoint_ = joint.shared_from_this ();
+    }
+
+  } // end of namespace rbdl.
+} // end of namespace ard.
